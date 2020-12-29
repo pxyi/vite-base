81e7ce5dc044a06d0abbae3b99a7cca581ff0e08
yuchangjiang <yuchangjiang@xiaohe.com>
2020-12-29 13:35:28 +0800

    样式修改

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -10,8 +10,8 @@ import Store from './../store';
 
 /* ------------------------- 默认请求格式, 和全局请求地址 ------------------------- */
 axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-// axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL as string;
-axios.defaults.baseURL = 'http://192.168.4.14:8080';
+axios.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL as string;
+// axios.defaults.baseURL = 'http://192.168.4.14:8080';
 axios.interceptors.request.use((res: AxiosRequestConfig) => {
   res.headers['accessToken'] = window.localStorage.getItem('token');
   res.headers['userId'] = Store.getters.userInfo ? Store.getters.userInfo.user.id : null;
