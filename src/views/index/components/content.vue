b82518c72206b97fb4150d32967b7a96a62981f2
yuchangjiang <yuchangjiang@xiaohe.com>
2021-01-06 14:24:22 +0800

    '题库bug修改'

--- src/views/index/components/content.vue
+++ src/views/index/components/content.vue
@@ -7,12 +7,15 @@
     <img class="m__head_bg" src="/@/assets/index/head-bg.png" alt="爱学标品" />
     <div class="m__menu">
       <div class="m__inline_title">快捷入口:</div>
-      <div class="m__menu_cell">备授课</div>
+      <!-- <div class="m__menu_cell">备授课</div>
       <div class="m__menu_cell">我的班级</div>
       <div class="m__menu_cell">题库</div>
       <div class="m__menu_cell" >资料库</div>
       <div class="m__menu_cell">试卷库</div>
-      <div class="m__menu_cell">课程管理</div>
+      <div class="m__menu_cell">课程管理</div> -->
+      <template  v-for="(menu) in list" :key="menu.key">
+        <div class="m__menu_cell" v-for="link in menu.children" :key="link.key" @click="$router.push(link.key)">{{ link.title }}</div>
+      </template>
     </div>
     <div class="m__section">
       <h4><span>教研统计</span><sub>(较昨日)</sub></h4>
@@ -53,13 +56,23 @@
 </template>
 <script lang="ts">
 import { useStore } from 'vuex';
+import MenuList, { RouterConf } from './../../../core/menu-list';
+import { useRoute } from 'vue-router';
 import RankingComponent from './ranking.vue';
+import { ref, watch } from 'vue'
 export default {
   name: 'index-content',
   components: { RankingComponent },
   props: { nickName: String },
   setup() {
-    return {  }
+    let list: { value: RouterConf[] } = ref([]);
+
+    let initPath = useRoute().path;
+    MenuList.map((res: any) => {
+      res.closed = initPath.includes(res.key);
+      list.value.push(res);
+    });
+    return { list, initPath }
   }
 }
 </script>
