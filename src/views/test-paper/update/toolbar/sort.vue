a0fe7cf4d548014ea4d1cdaf489f3be892769cf0
win <1834262609@qq.com>
2021-01-14 20:15:24 +0800

    requestAnimationFrame of scrollTop

--- src/views/test-paper/update/toolbar/sort.vue
+++ src/views/test-paper/update/toolbar/sort.vue
@@ -52,7 +52,7 @@ export default {
 
     const scrollTo = (idx, index) => {
       let top = (document.querySelector(`.paper_content .item[data-uuid="${idx}-${index}"]`) as HTMLElement).offsetTop;
-      ScrollTop(document.querySelector('.paper_content'), top, 200);
+      ScrollTop(document.querySelector('.paper_content'), top, 50, 2000);
     }
 
     return { paperCharpts, toChinesNum, sortHandle, sortChange, scrollTo }
